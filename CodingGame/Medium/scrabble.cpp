effc4a80ccf436132b010ed69f95a5c545ae9e75
Alexander <cool.ipatovo@gmail.com>
2017-02-06 13:49:29 +0300

    Huge optimization was performed on scrabble!

--- CodingGame/Medium/scrabble.cpp
+++ CodingGame/Medium/scrabble.cpp
@@ -1,99 +1,107 @@
 #include <iostream>
 #include <string>
+#include <array>
 #include <vector>
 #include <map>
 #include <algorithm>
 
 using namespace std;
-typedef map<char,int> letter_map_type;
-typedef vector<string> dictionary_type;
-letter_map_type letter_cost_map;
 
-void init_letters_costs()
+const static int max_letter_cost = 10;
+static int max_word_length = 0;
+
+class Letter
 {
-    letter_cost_map.clear();
-    letter_cost_map.insert(std::pair<char,int>('e',1));
-    letter_cost_map.insert(std::pair<char,int>('a',1));
-    letter_cost_map.insert(std::pair<char,int>('i',1));
-    letter_cost_map.insert(std::pair<char,int>('o',1));
-    letter_cost_map.insert(std::pair<char,int>('n',1));
-    letter_cost_map.insert(std::pair<char,int>('r',1));
-    letter_cost_map.insert(std::pair<char,int>('t',1));
-    letter_cost_map.insert(std::pair<char,int>('l',1));
-    letter_cost_map.insert(std::pair<char,int>('s',1));
-    letter_cost_map.insert(std::pair<char,int>('u',1));
-    
-    letter_cost_map.insert(std::pair<char,int>('d',2));
-    letter_cost_map.insert(std::pair<char,int>('g',2));
-    
-    letter_cost_map.insert(std::pair<char,int>('b',3));
-    letter_cost_map.insert(std::pair<char,int>('c',3));
-    letter_cost_map.insert(std::pair<char,int>('m',3));
-    letter_cost_map.insert(std::pair<char,int>('p',3));
-    
-    letter_cost_map.insert(std::pair<char,int>('f',4));
-    letter_cost_map.insert(std::pair<char,int>('h',4));
-    letter_cost_map.insert(std::pair<char,int>('v',4));
-    letter_cost_map.insert(std::pair<char,int>('w',4));
-    letter_cost_map.insert(std::pair<char,int>('y',4));
-    
-    letter_cost_map.insert(std::pair<char,int>('k',5));
-    
-    letter_cost_map.insert(std::pair<char,int>('j',8));
-    letter_cost_map.insert(std::pair<char,int>('x',8));
-    
-    letter_cost_map.insert(std::pair<char,int>('q',10));
-    letter_cost_map.insert(std::pair<char,int>('z',10));
-}
+public:
+	Letter() {}
+	Letter(const int cost) : 
+		letter_cost(cost), occurence(0), query_id(0), current_occurence(0)
+	{
+	}
+	void reached()
+	{
+		++occurence;
+	}
+	int get_cost(const int qr_id)
+	{
+		if (qr_id != query_id)
+		{
+			query_id = qr_id;
+			current_occurence = occurence;
+		}
+		if (current_occurence == 0)
+			return -max_letter_cost * max_word_length;
+		--current_occurence;
+		return letter_cost;
+	}
 
-int cost_str( const string& str)
+private:
+	int letter_cost;
+	int occurence;
+
+	int query_id;
+	int current_occurence;
+};
+
+typedef array<Letter, 26> pattern_type;
+
+static pattern_type pattern = 
 {
-    int cost = 0;
-    for ( size_t i = 0; i < str.size(); ++i )
-        cost += letter_cost_map[str[i]];
-    return cost;
-}
+	Letter(1),	//	a
+	Letter(3),	//	b
+	Letter(3),	//	c
+	Letter(2),	//	d
+	Letter(1),	//	e
+	Letter(4),	//	f
+	Letter(2),	//	g
+	Letter(4),	//	h
+	Letter(1),	//	i
+	Letter(8),	//	j
+	Letter(5),	//	k
+	Letter(1),	//	l
+	Letter(3),	//	m
+	Letter(1),	//	n
+	Letter(1),	//	o
+	Letter(3),	//	p
+	Letter(10),	//	q
+	Letter(1),	//	r
+	Letter(1),	//	s
+	Letter(1),	//	t
+	Letter(1),	//	u
+	Letter(4),	//	v
+	Letter(4),	//	w
+	Letter(8),	//	x
+	Letter(4),	//	y
+	Letter(10),	//	z
+};
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 int main()
 {
-    init_letters_costs();
-    dictionary_type dictionary;
-    int N;
-    cin >> N; cin.ignore();
-    int max_cost = 0;
-    for (int i = 0; i < N; i++) {
-        string W;
-        getline(cin, W);
-        dictionary.push_back( W );
-    }
-    for ( size_t i = 0; i < dictionary.size(); ++i )
-        cerr << "str: " << dictionary[i] << " cost : " << cost_str(dictionary[i]) <<endl;
-    string LETTERS;
-    getline(cin, LETTERS);
-    int max_score = 0;
-    string answer;
-    for ( size_t i = 0; i<dictionary.size(); ++i )
-    {
-        string copy_str = dictionary[i];
-        int current_score = cost_str(copy_str);
-        for ( size_t j = 0; j < LETTERS.size(); ++j )
-        {
-            if (copy_str.empty())
-                break;
-            size_t pos = copy_str.find(LETTERS[j]);
-            if ( pos == string::npos )
-                continue;
-            copy_str.erase( copy_str.begin() + pos );
-        }
-        if ( copy_str.empty() && max_score < current_score )
-        {
-            max_score = current_score;
-            answer = dictionary[i];
-        }
-    }
-    cout << answer << endl;
+	int N;
+	cin >> N; cin.ignore();
+	int index = 0;
+	int max_cost = -10;
+	vector<string> words(N, string());
+	for (int i = 0; i < N; ++i)
+	{
+		getline(cin, words[i]);
+		max_word_length = max<int>(max_word_length, words[i].size());
+	}
+	string LETTERS;
+	getline(cin, LETTERS);
+	for (int i = 0; i < LETTERS.size(); ++i)
+		pattern[LETTERS[i] - 'a'].reached();
+
+	for (int i = 0, query_id = 1; i < words.size(); ++i, ++query_id)
+	{
+		int current_cost = 0;
+		for (int j = 0; 0 <= current_cost && j < words[i].size(); ++j)
+			current_cost += pattern[words[i][j] - 'a'].get_cost(query_id);
+		if (max_cost < current_cost)
+		{
+			index = i;
+			max_cost = current_cost;
+		}
+	}
+	cout << words[index] << endl;
 }
