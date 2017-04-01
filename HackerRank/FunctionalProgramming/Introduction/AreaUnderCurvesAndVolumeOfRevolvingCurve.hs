4d05dc42b0762f948d5067143604f98ce8cfea1f
LmTinyToon <cool.ipatovo@gmail.com>
2017-04-01 18:33:16 +0300

    Computation of integral was fixed

--- HackerRank/FunctionalProgramming/Introduction/AreaUnderCurvesAndVolumeOfRevolvingCurve.hs
+++ HackerRank/FunctionalProgramming/Introduction/AreaUnderCurvesAndVolumeOfRevolvingCurve.hs
@@ -9,7 +9,7 @@ steps_count s = round $ (fromIntegral s) / step
 
 integrate :: (Double -> Double) -> Double -> Int -> Double
 integrate f a 0 = 0
-integrate f a i = (f a) + (integrate f (a + step) (i - 1))
+integrate f a i = (f a) * step + (integrate f (a + step) (i - 1))
 
 --  Generation of function
 gen_func :: [Double] -> [Double] -> (Double -> Double)
@@ -17,9 +17,10 @@ gen_func a b = \x -> sum (zipWith (*) a (map (x**) b))
 
 -- This function should return a list [area, volume].
 solve :: Int -> Int -> [Int] -> [Int] -> [Double]
-solve l r a b = [integr_res, pi * integr_res] where 
+solve l r a b = [area, volume] where 
                     f = gen_func (map fromIntegral a) (map fromIntegral b)
-                    integr_res = integrate f (fromIntegral l) (steps_count (r-l))
+                    area = integrate f (fromIntegral l) (steps_count (r-l))
+                    volume = pi * integrate (\x -> (f x) * (f x)) (fromIntegral l) (steps_count (r-l))
 
 --Input/Output.
 main :: IO ()
