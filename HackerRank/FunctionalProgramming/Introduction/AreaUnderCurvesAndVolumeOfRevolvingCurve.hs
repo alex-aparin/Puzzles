c34da9bdbcfca58da64d994bf0219b01a682c217
LmTinyToon <cool.ipatovo@gmail.com>
2017-04-01 18:24:25 +0300

    Minor fixes

--- HackerRank/FunctionalProgramming/Introduction/AreaUnderCurvesAndVolumeOfRevolvingCurve.hs
+++ HackerRank/FunctionalProgramming/Introduction/AreaUnderCurvesAndVolumeOfRevolvingCurve.hs
@@ -2,24 +2,24 @@
 
 --  Integro functions
 step :: Double
-step = 0.002
+step = 0.001
 
 steps_count :: Int -> Int
 steps_count s = round $ (fromIntegral s) / step
 
-integrate :: (Double -> Double) -> Double -> Double -> Int -> Double
-integrate f a h 0 = 0
-integrate f a h i = (f a) + (integrate f (a + h) h (i - 1))
+integrate :: (Double -> Double) -> Double -> Int -> Double
+integrate f a 0 = 0
+integrate f a i = (f a) + (integrate f (a + step) (i - 1))
 
 --  Generation of function
 gen_func :: [Double] -> [Double] -> (Double -> Double)
-gen_func a b = \x -> sum (zipWith (*) a (map (**x) b))
+gen_func a b = \x -> sum (zipWith (*) a (map (x**) b))
 
 -- This function should return a list [area, volume].
 solve :: Int -> Int -> [Int] -> [Int] -> [Double]
 solve l r a b = [integr_res, pi * integr_res] where 
                     f = gen_func (map fromIntegral a) (map fromIntegral b)
-                    integr_res = integrate f (fromIntegral l) step (steps_count (r-l))
+                    integr_res = integrate f (fromIntegral l) (steps_count (r-l))
 
 --Input/Output.
 main :: IO ()
