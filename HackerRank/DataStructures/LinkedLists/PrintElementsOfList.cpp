7b68e6123442f04d8187d5bc6c8112cd72591ae7
Alexander <cool.ipatovo@gmail.com>
2017-04-07 08:29:34 +0300

    Printing of elements of list was implemented

---
+++ HackerRank/DataStructures/LinkedLists/PrintElementsOfList.cpp
@@ -0,0 +1,15 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+  for (; head != 0; head = head->next)
+      cout << head->data << endl;
+}
