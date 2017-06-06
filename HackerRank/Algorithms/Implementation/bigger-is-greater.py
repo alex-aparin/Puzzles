30471efdf45e121963b4c9d5575939c1490e2fd5
Alexander <cool.ipatovo@gmail.com>
2017-06-06 18:02:35 +0300

    bigger-is-greater puzzle was solved

--- HackerRank/Algorithms/Implementation/bigger-is-greater.py
+++ HackerRank/Algorithms/Implementation/bigger-is-greater.py
@@ -1,27 +1,16 @@
-#   Aux funcs
-def has_greater_str(s):
-    c = s[0]
-    for x in s[1:]:
-        if c < x:
-            return True
-        c = x
-    return False
-
 def answer(s):
-    if has_greater_str(s) == False:
-        return "no answer"
     i = len(s) - 2
     while i >= 0:
         val_index, j = -1, len(s) - 1
         while i < j:
             if s[i] < s[j]:
-                if val_index == -1 or s[val_index] < s[j]:
+                if val_index == -1 or s[j] < s[val_index]:
                     val_index = j
             j -= 1
         if i < val_index:
             chars = list(s)
             chars[i], chars[val_index] = chars[val_index], chars[i]
-            return ''.join(chars)
+            return ''.join(chars[:i+1] + sorted(chars[i+1:]))
         i -= 1
     return "no answer"
         
