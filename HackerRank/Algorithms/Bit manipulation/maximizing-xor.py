bb392fa20b9f7adf84ab0bc03161b942e1b49ec5
Alexander <cool.ipatovo@gmail.com>
2017-12-25 17:33:29 +0300

    Solution was optimized

--- HackerRank/Algorithms/Bit manipulation/maximizing-xor.py
+++ HackerRank/Algorithms/Bit manipulation/maximizing-xor.py
@@ -1,17 +1,3 @@
-a, b = int(input()), int(input())
-if a < b:
-    a, b = b, a
-a, b = list(format(a, 'b')), list(format(b, 'b'))
-b = ['0'] * (len(a) - len(b)) + b
-i = 0
-while i < len(a):
-    if a[i] == b[i] == '1':
-        a[i] = '0'
-        i += 1
-        continue
-    if a[i] == '1' and b[i] == '0':
-        while i < len(a):
-            a[i] = '1'
-            i += 1
-    i += 1
-print(int("".join(a), 2))
+import math
+res = (1 << math.floor(math.log(int(input()) ^ int(input()), 2) + 1)) - 1
+print(res)
